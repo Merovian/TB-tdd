e927a55b1d8beb3681f6d9c40d88f5ee9f0c9f71
Will Cowen <will.cowen@gmail.com>
2012-07-16 10:07:12 -0400

    cucumber reader

---
+++ todos/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todos::Application.config.secret_token = 'bb68054be32430f73c265057a75fc513d38d3ec733217b44b6a4229af49bbcc323a6a483e6235e61c48e2ad75835e5d168659177c3c3f02d5fbd34776ee8db8d'
