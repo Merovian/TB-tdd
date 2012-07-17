26d60066f7bbbb4dea9718617219bdb0958afdc2
Will Cowen <will.cowen@gmail.com>
2012-07-17 09:54:43 -0400

    START HASHTAG (rails new hashtag --skip-test-unit)

---
+++ hashtag/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hashtag::Application.config.secret_token = '2b00bcdc7db074d807b85da01b57f4208728630d6e1da0e8cace8a8ea1220154023bc99eb4bf9ecb605c2d3693e24fd5e48641bb987c3a1580e36dd6127c9dae'
